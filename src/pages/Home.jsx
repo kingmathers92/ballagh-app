b8cdd8109d65e9839d26584a01fda9a93a99a076
khaled <khaledb.yahya@gmail.com>
2025-05-27 22:21:14 +0100

    added hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,30 @@
 import { Link } from "react-router-dom";
-// Import Font Awesome icons
 import { FaSearch, FaRandom, FaKaaba } from "react-icons/fa";
 
 function Home() {
   return (
-    <div className="container">
-      <h1 className="title">بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ</h1>
-      <h2 className="subtitle">Welcome to the Quran App</h2>
-      <p className="description">
-        Explore the Holy Quran, search for verses, listen to recitations, or
-        find the Qibla direction with ease.
-      </p>
-      <div className="buttons-container">
-        <Link to="/search" className="button">
-          <FaSearch className="button-icon" /> Search Verses
-        </Link>
-        <Link to="/random" className="button">
-          <FaRandom className="button-icon" /> Get Random Verse
-        </Link>
-        <Link to="/qibla" className="button">
-          <FaKaaba className="button-icon" /> Qibla Direction
-        </Link>
+    <div>
+      <div className="hero-section">
+        <h1 className="title">بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ</h1>
+      </div>
+      <div className="container">
+        <h2 className="subtitle">Welcome to the Quran App</h2>
+        <p className="description">
+          Discover the beauty of the Holy Quran. Read verses, listen to soothing
+          recitations, search for specific ayahs, or find the Qibla direction to
+          enhance your spiritual journey.
+        </p>
+        <div className="buttons-container">
+          <Link to="/search" className="button">
+            <FaSearch className="button-icon" /> Search Verses
+          </Link>
+          <Link to="/random" className="button">
+            <FaRandom className="button-icon" /> Get Random Verse
+          </Link>
+          <Link to="/qibla" className="button">
+            <FaKaaba className="button-icon" /> Qibla Direction
+          </Link>
+        </div>
       </div>
     </div>
   );

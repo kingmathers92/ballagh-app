bd986f200a8a4b23aa9a7b081ca13cd4c44f4097
khaled <khaledb.yahya@gmail.com>
2025-06-18 23:13:13 +0100

    Extracted reminder into reminderUtils and integrated it with usePrayerTimes

---
+++ src/utils/reminderUtils.js
@@ -0,0 +1,16 @@
+export const scheduleReminders = (nextEvent, updateTimes) => {
+  if (nextEvent.name === "Suhoor" && nextEvent.time > new Date()) {
+    const timeUntilSuhoor = nextEvent.time - new Date();
+    if (timeUntilSuhoor > 15 * 60 * 1000) {
+      setTimeout(
+        () => alert("Suhoor ends at Fajr in 15 minutes!"),
+        timeUntilSuhoor - 15 * 60 * 1000
+      );
+    }
+  } else if (nextEvent.name === "Iftar" && nextEvent.time > new Date()) {
+    setTimeout(
+      () => alert("Time for Iftar is approaching!"),
+      nextEvent.time - new Date() - 5 * 60 * 1000
+    );
+  }
+};

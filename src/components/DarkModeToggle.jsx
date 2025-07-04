430830dd644f1a9ebc7580f7afdf29f72b6a7094
khaled <khaledb.yahya@gmail.com>
2025-07-04 18:26:57 +0100

    Improved Accessibility in DarkModeToggle

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -2,7 +2,11 @@ import PropTypes from "prop-types";
 
 const DarkModeToggle = ({ theme, toggleTheme }) => {
   return (
-    <button onClick={toggleTheme} className="dark-mode-toggle">
+    <button
+      onClick={toggleTheme}
+      className="dark-mode-toggle"
+      aria-label={`Switch to ${theme === "light" ? "dark" : "light"} mode`}
+    >
       {theme === "light" ? (
         <span role="img" aria-label="dark mode">
           🌙

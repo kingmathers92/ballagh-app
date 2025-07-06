0d9532412c3d53cf6570af14ea342d4c5d932979
khaled <khaledb.yahya@gmail.com>
2025-07-06 18:41:23 +0100

    Updated scheduleReminders to Use Notification State

--- src/utils/reminderUtils.js
+++ src/utils/reminderUtils.js
@@ -1,17 +1,16 @@
-export const scheduleReminders = (nextEvent) => {
+export const scheduleReminders = (nextEvent, addNotification) => {
   if (nextEvent.name === "Suhoor" && nextEvent.time > new Date()) {
     const timeUntilSuhoor = nextEvent.time - new Date();
     if (timeUntilSuhoor > 15 * 60 * 1000) {
       setTimeout(
-        () => alert("Suhoor ends at Fajr in 15 minutes!"),
+        () => addNotification("Suhoor ends at Fajr in 15 minutes!"),
         timeUntilSuhoor - 15 * 60 * 1000
       );
     }
   } else if (nextEvent.name === "Iftar" && nextEvent.time > new Date()) {
     setTimeout(
-      () => alert("Time for Iftar is approaching!"),
+      () => addNotification("Time for Iftar is approaching!"),
       nextEvent.time - new Date() - 5 * 60 * 1000
     );
   }
 };
-// REMINDWER STYLING IMPORVEMENGTS!!!!!!!

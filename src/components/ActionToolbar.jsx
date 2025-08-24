b2e60b9b1b533ef1ce6e53c023452a4fcb6020cf
khaled <khaledb.yahya@gmail.com>
2025-08-24 14:00:35 +0100

    Added PropTypes to ActionToolbar.jsx

--- src/components/ActionToolbar.jsx
+++ src/components/ActionToolbar.jsx
@@ -1,4 +1,5 @@
 import { memo } from "react";
+import PropTypes from "prop-types";
 import {
   requestNotificationPermission,
   dismissAllNotifications,
@@ -73,5 +74,45 @@ const ActionToolbar = ({
     </button>
   </div>
 );
+
+ActionToolbar.propTypes = {
+  notificationPermission: PropTypes.oneOf(["default", "granted", "denied"])
+    .isRequired,
+  setNotificationPermission: PropTypes.func.isRequired,
+  notifications: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      message: PropTypes.string.isRequired,
+      isPermissionMessage: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+  setNotifications: PropTypes.func.isRequired,
+  prayerTimes: PropTypes.shape({
+    fajr: PropTypes.instanceOf(Date),
+    sunrise: PropTypes.instanceOf(Date),
+    dhuhr: PropTypes.instanceOf(Date),
+    asr: PropTypes.instanceOf(Date),
+    maghrib: PropTypes.instanceOf(Date),
+    isha: PropTypes.instanceOf(Date),
+    location: PropTypes.shape({
+      latitude: PropTypes.number,
+      longitude: PropTypes.number,
+    }),
+  }),
+  ramadanTimes: PropTypes.shape({
+    ramadanDay: PropTypes.number,
+    suhoor: PropTypes.instanceOf(Date),
+    iftar: PropTypes.instanceOf(Date),
+    currentEvent: PropTypes.string,
+    nextEvent: PropTypes.shape({
+      name: PropTypes.string,
+      time: PropTypes.instanceOf(Date),
+    }),
+  }),
+  calculationMethod: PropTypes.string.isRequired,
+  timeZone: PropTypes.string.isRequired,
+  language: PropTypes.string.isRequired,
+  translations: PropTypes.object.isRequired,
+};
 
 export default memo(ActionToolbar);
